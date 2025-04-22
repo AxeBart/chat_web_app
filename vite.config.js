5ffc6d76dbc37543c4552659e069f443cf0cf77a
Barth Lungele <barthlungele@gmail.com>
2025-04-22 13:07:55 +0100

    pff

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['socket.io-client']
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
